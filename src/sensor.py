8fc71f84deb5a59feae4df98102afe34703cebd9
as-1212 <adrizasrivastava06@gmail.com>
2026-04-08 23:52:01 +0530

    Delete src directory

--- src/sensor.py
+++
@@ -1,9 +0,0 @@
-import board
-import busio
-import adafruit_adxl34x
-
-i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
-
-def get_motion():
-    return accelerometer.acceleration
